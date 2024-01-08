fd14498041a942132acf79c2431844622bb810bf
EKayatz <eliaskayatz04@gmail.com>
2024-01-08 08:40:37 +0100

    ...

---
+++ src/main/java/com/ekayatz/chat/confiig/WebSocketEventListener.java
@@ -0,0 +1,14 @@
+package com.ekayatz.chat.confiig;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+
+public class WebSocketEventListener {
+	
+}
